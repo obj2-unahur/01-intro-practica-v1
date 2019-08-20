0b269d4c24cd14e4fbd9b1c0d3dfa2d58b33369e
Darío Hruszecki <dhruszecki@despegar.com>
2019-08-20 20:42:18 -0300

    base

---
+++ src/main/java/ar.edu.unahur.obj2.ejercicio1/Ejercicio1.java
@@ -0,0 +1,8 @@
+package ar.edu.unahur.obj2.ejercicio1;
+
+public class Ejercicio1 {
+
+    public int[] ordenar() {
+        return new int[] {};
+    }
+}
---
+++ src/test/java/ar/edu/unahur/obj2/ejercicio1/Ejercicio1Test.java
@@ -0,0 +1,12 @@
+package ar.edu.unahur.obj2.ejercicio1;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class Ejercicio1Test {
+
+    @Test
+    public void testOrdenar() {
+    }
+}
